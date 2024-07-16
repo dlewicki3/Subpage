ac9d26223cee2b04542a81e664a653544b85e336
Dominik Lewicki <dominik_03-06@wp.pl>
2024-07-16 15:37:05 +0100

    firt

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import O1 from "./O1.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <O1/>
+     </div>
+  );
+}
+
+export default App;
---
+++ src/O1.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./O1.css";
+
+function O1() {
+  return (
+<div className="o1">
+    <div className="k1">
+
+    </div>
+</div>
+  )
+}
+
+export default O1
